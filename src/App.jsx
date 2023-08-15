7e9517a3b81241a1ff0d8c336f41335c5d8a9b92
Joel Perez <perezjoeldario@gmail.com>
2023-08-15 10:18:15 -0300

    segunda subida

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./navbar";
@@ -11,24 +10,6 @@ function App() {
       <Navbar /> {/* Movemos el Navbar fuera del contenedor d-flex */}
       <Carousel />
       <Franja />
-=======
-/* import { useState } from "react";
- */ import Navbar from "./navbar";
-import "./App.css";
-import Carousel from "./carousel";
-import Funcionalidades from "./funcionalidades";
-import Optimiza from "./optimiza";
-
-function App() {
-  /*   const [count, setCount] = useState(0);
-   */
-  return (
-    <>
-      <Navbar />
-      <Carousel />
-      <Funcionalidades />
-      <Optimiza />
->>>>>>> f6a757a (Primer subida pagina web)
     </>
   );
 }
